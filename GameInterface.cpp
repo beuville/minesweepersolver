ca8da0c92a6e00de49ce6443721934aebb203de6
Beau.Furrow <Beau.Furrow@7e498df5-8955-0410-9177-9339b6f37624>
2008-10-23 01:39:13 +0000

    Added Skeleton of game interface

---
+++ GameInterface.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//#include "Gameboard.h"
+#include "GameInterface.h"
+using namespace std;
+
+class GameInterface{
+	float reportSpecs(){
+		
+	}
+
+	/* actual game commands inherited from game board */
+	int sendCommand(){
+
+	}
+
+	report_specs report(){
+		
+	}
+
+	int initGame(string difficulty){
+		
+	}
+}
